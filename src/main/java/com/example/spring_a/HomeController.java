fa5b75e375b29270f415cb241e4c8708724331b5
Ms-Shoshany <channashosh@gmail.com>
2025-12-31 10:23:59 +0200

    Add file via API

---
+++ src/main/java/com/example/spring_a/HomeController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+
+	@GetMapping("/hello")
+    public Map<String, Object> hello(
+        ) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("message", "Hello, from spring-a//hello!");
+        return data; // automatically converted to JSON
+        }
+
+	@GetMapping("/hey")
+    public Map<String, Object> hey(
+        ) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("message", "Hello, from spring-a//hey!");
+        return data; // automatically converted to JSON
+        }
+
+    
+}
